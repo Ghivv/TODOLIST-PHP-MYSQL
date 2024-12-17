5458871c637d4f5f55d4966245e3fe031bc7ed4b
GHIVARI MGP <fahripardiansyah@gmail.com>
2024-12-18 05:42:14 +0700

    perbaikan

--- Config/Database.php
+++ Config/Database.php
@@ -12,7 +12,7 @@ namespace Config {
             $username = "root";
             $password = "";
 
-            return new \PDO("mysql:host=$host;$port;dbname=$database", $username, $password);
+            return new \PDO("mysql:host=$host:$port;dbname=$database", $username, $password);
         }
     }
 }
